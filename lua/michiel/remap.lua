a4cc5737af2f155143ddadf689f7989f62eabf50
MichielN19 <Michielmitsjol@gmail.com>
2025-09-16 20:17:21 +0200

    Added config so the errors are default on

--- lua/michiel/remap.lua
+++ lua/michiel/remap.lua
@@ -50,23 +50,30 @@ vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
 end)
 
+vim.diagnostic.config({
+    virtual_text = {
+        source = true,
+        format = function (diagnostic)
+            return string.format("%s: %s", diagnostic.source, diagnostic.message)
+        end,
+    }
+})
+
 local function toggle_diagnostics_display()
     local current_config = vim.diagnostic.config()
     local is_enabled = current_config.virtual_text ~= false
-    
     if is_enabled then
         vim.diagnostic.config({ virtual_text = false })
     else
         vim.diagnostic.config({
             virtual_text = {
-                source = "always",
+                source = true,
                 format = function (diagnostic)
                     return string.format("%s: %s", diagnostic.source, diagnostic.message)
                 end,
             }
         })
     end
-    
 end
 
 vim.keymap.set("n", "<leader>e", toggle_diagnostics_display)
